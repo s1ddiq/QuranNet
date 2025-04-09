1b96fb3aaa9895781b7bb6557b2b10a5f780eb7e
s1ddiq <siddiq.nasheed@gmail.com>
2025-04-09 02:41:30 +0200

    feat: QuranNet navigates to home onclick

--- components/sidebar/SidebarHeader.tsx
+++ components/sidebar/SidebarHeader.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SidebarHeader = ({ toggleSidebar, isCollapsed }: SidebarHeaderProps) => {
+  const router = useRouter();
   return (
     <div
       className="flex w-full lg:min-h-[64px] md:min-h-8 px-4 justify-between items-center lg:mt-0 mt-4 lg:border-b border-[#262629ff] transition-all duration-300 hide-on-scroll"
     >
-      <p className={`text-xl font-bold ${isCollapsed && "hidden"}`}>QuranNet</p>
+      <p className={`text-xl font-bold cursor-pointer ${isCollapsed && "hidden"}`} onClick={() => router.push('/')} title="QuranNet - Home">QuranNet</p>
       <Image
         src="/svg/menu.svg"
         width={32}
